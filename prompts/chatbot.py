91ef9d1a6f8ebba7fa39a14e28bec800b7315c0a
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-09-06 14:51:38 +0530

    Added the prompt in langchain

--- prompts/chatbot.py
+++ prompts/chatbot.py
@@ -1,3 +1,4 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
@@ -5,15 +6,19 @@ load_dotenv()
 
 model = ChatOpenAI()
 
-chat_history = []
+chat_history = [
+    SystemMessage(
+        content = "You are a helpful AI assistant"
+    )
+]
 
 while True:
     user_input = input("You: ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content = user_input))
     if user_input == 'exit':
         break
     result = model.invoke(chat_history)
-    chat_history.append(result.append)
+    chat_history.append(AIMessage(content = result.append))
     print("AI: ", result.content)
 
 print(chat_history)
